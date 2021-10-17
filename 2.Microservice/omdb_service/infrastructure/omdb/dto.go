596e6cc08e87fd6b3edecf87b1cdfc4a4cbdb254
HendroPrabowo <awebened@gmail.com>
2021-10-18 00:47:19 +0700

    update omdb dto

--- 2.Microservice/omdb_service/infrastructure/omdb/dto.go
+++ 2.Microservice/omdb_service/infrastructure/omdb/dto.go
@@ -21,6 +21,35 @@ type Filter struct {
 }
 
 type MovieInformation struct {
-	Response string `json:"Response"`
-	Error    string `json:"Error,omitempty"`
+	Title      string `json:"Title"`
+	Year       string `json:"Year"`
+	Rated      string `json:"Rated"`
+	Released   string `json:"Released"`
+	Runtime    string `json:"Runtime"`
+	Genre      string `json:"Genre"`
+	Director   string `json:"Director"`
+	Writer     string `json:"Writer"`
+	Actors     string `json:"Actors"`
+	Plot       string `json:"Plot"`
+	Language   string `json:"Language"`
+	Country    string `json:"Country"`
+	Awards     string `json:"Awards"`
+	Poster     string `json:"Poster"`
+	Ratings    Rating `json:"Ratings"`
+	Metascore  string `json:"Metascore"`
+	ImdbRating string `json:"imdbRating"`
+	ImdbVotes  string `json:"imdbVotes"`
+	ImdbID     string `json:"imdbID"`
+	Type       string `json:"Type"`
+	DVD        string `json:"DVD"`
+	BoxOffice  string `json:"BoxOffice"`
+	Production string `json:"Production"`
+	Website    string `json:"Website"`
+	Response   string `json:"Response"`
+	Error      string `json:"Error,omitempty"`
+}
+
+type Rating struct {
+	Source string `json:"Source"`
+	Value  string `json:"Value"`
 }
